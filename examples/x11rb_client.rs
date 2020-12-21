21fae85b5892c9abf4f25d81a6bdadc211c33a93
Riey <creeper844@gmail.com>
2020-12-22 03:19:20 +0900

    Impl ClientHandler

--- examples/x11rb_client.rs
+++ examples/x11rb_client.rs
@@ -1,9 +1,102 @@
-use std::convert::TryFrom;
-
 use x11rb::connection::Connection;
 use x11rb::protocol::{xproto::*, Event};
-use xim::x11rb::{Client, ClientError};
-use xim_parser::{AttributeName, CommitData, InputStyle, Request, Spot};
+use xim::{
+    x11rb::{ClientError, X11rbClient},
+    Client, ClientHandler,
+};
+use xim_parser::{AttributeName, ForwardEventFlag, InputStyle, Spot};
+
+#[derive(Default)]
+struct ExampleHandler {
+    im_id: u16,
+    ic_id: u16,
+    connected: bool,
+    window: u32,
+}
+
+impl<C: Client> ClientHandler<C> for ExampleHandler {
+    fn handle_connect(&mut self, client: &mut C) -> Result<(), C::Error> {
+        log::trace!("Connected");
+        self.connected = true;
+        client.open(b"en_US")
+    }
+
+    fn handle_open(&mut self, client: &mut C, input_method_id: u16) -> Result<(), C::Error> {
+        log::trace!("Opened");
+        self.im_id = input_method_id;
+        let ic_attributes = client
+            .build_ic_attributes()
+            .push(
+                AttributeName::InputStyle,
+                InputStyle::PREEDITPOSITION | InputStyle::STATUSNOTHING,
+            )
+            .push(AttributeName::ClientWindow, self.window)
+            .push(AttributeName::FocusWindow, self.window)
+            .nested_list(AttributeName::PreeditAttributes, |b| {
+                b.push(AttributeName::SpotLocation, Spot { x: 0, y: 0 });
+            })
+            .build();
+        client.create_ic(input_method_id, ic_attributes)
+    }
+
+    fn handle_query_extension(
+        &mut self,
+        _client: &mut C,
+        _extensions: &[xim_parser::Extension],
+    ) -> Result<(), C::Error> {
+        Ok(())
+    }
+
+    fn handle_create_ic(
+        &mut self,
+        _client: &mut C,
+        input_method_id: u16,
+        input_context_id: u16,
+    ) -> Result<(), C::Error> {
+        log::info!("IC created {}, {}", input_method_id, input_context_id);
+        Ok(())
+    }
+
+    fn handle_commit(
+        &mut self,
+        _client: &mut C,
+        _input_method_id: u16,
+        _input_context_id: u16,
+        text: &str,
+    ) -> Result<(), C::Error> {
+        log::info!("Commited {}", text);
+        Ok(())
+    }
+
+    fn handle_disconnect(&mut self) {
+        log::info!("disconnected");
+    }
+
+    fn handle_close(&mut self, client: &mut C, _input_method_id: u16) -> Result<(), C::Error> {
+        log::info!("closed");
+        client.disconnect()
+    }
+
+    fn handle_destory_ic(
+        &mut self,
+        client: &mut C,
+        input_method_id: u16,
+        _input_context_id: u16,
+    ) -> Result<(), C::Error> {
+        client.close(input_method_id)
+    }
+
+    fn handle_forward_event(
+        &mut self,
+        _client: &mut C,
+        _input_method_id: u16,
+        _input_context_id: u16,
+        _flag: xim_parser::ForwardEventFlag,
+        _xev: xim_parser::RawXEvent,
+    ) -> Result<(), C::Error> {
+        Ok(())
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     pretty_env_logger::init();
@@ -29,156 +122,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     conn.map_window(window)?;
     conn.flush()?;
 
-    let mut client = Client::init(&conn, screen, None)?;
+    let mut client = X11rbClient::init(&conn, screen, None)?;
 
     log::info!("Start event loop");
 
-    let mut im_id = None;
-    let mut ic_id = None;
+    let mut handler = ExampleHandler::default();
+    handler.window = window;
 
-    let mut end = false;
-
-    while !end {
+    loop {
         let e = conn.wait_for_event()?;
 
-        if client.filter_event(&e, |client, req| {
-            log::trace!("Recv req: {:?}", req);
-
-            match req {
-                Request::ConnectReply {
-                    server_major_protocol_version: _,
-                    server_minor_protocol_version: _,
-                } => client.send_req(Request::Open {
-                    locale: "en_US".into(),
-                }),
-                Request::OpenReply {
-                    input_method_id,
-                    im_attrs,
-                    ic_attrs,
-                } => {
-                    client.set_attrs(im_attrs, ic_attrs);
-                    client.send_req(Request::QueryExtension {
-                        input_method_id,
-                        extensions: vec![],
-                    })
-                }
-                Request::QueryExtensionReply {
-                    input_method_id, ..
-                } => client.send_req(Request::EncodingNegotiation {
-                    encodings: vec!["COMPOUND_TEXT".into(), "".into()],
-                    encoding_infos: vec![],
-                    input_method_id,
-                }),
-                Request::EncodingNegotiationReply {
-                    category: _,
-                    index: _,
-                    input_method_id,
-                } => {
-                    let ic_attributes = client
-                        .build_ic_attributes()
-                        .push(
-                            AttributeName::InputStyle,
-                            InputStyle::PREEDITPOSITION | InputStyle::STATUSNOTHING,
-                        )
-                        .push(AttributeName::ClientWindow, window)
-                        .push(AttributeName::FocusWindow, window)
-                        .nested_list(AttributeName::PreeditAttributes, |b| {
-                            b.push(AttributeName::SpotLocation, Spot { x: 0, y: 0 });
-                        })
-                        .build();
-
-                    client.send_req(Request::CreateIc {
-                        input_method_id,
-                        ic_attributes,
-                    })
-                }
-                Request::CreateIcReply {
-                    input_method_id,
-                    input_context_id,
-                } => {
-                    log::info!(
-                        "IC Created im: {}, ic: {}",
-                        input_method_id,
-                        input_context_id
-                    );
-
-                    im_id = Some(input_method_id);
-                    ic_id = Some(input_context_id);
-
-                    client.send_req(Request::SetIcFocus {
-                        input_method_id,
-                        input_context_id,
-                    })
-                }
-                Request::GetIcValuesReply {
-                    input_method_id: _,
-                    input_context_id: _,
-                    ic_attributes: _,
-                } => Ok(()),
-                Request::SetEventMask {
-                    input_method_id: _,
-                    input_context_id: _,
-                    forward_event_mask,
-                    synchronous_event_mask,
-                } => {
-                    client.set_event_mask(forward_event_mask, synchronous_event_mask);
-                    Ok(())
-                }
-                Request::CloseReply { input_method_id: _ } => {
-                    client.send_req(Request::Disconnect {})
-                }
-                Request::DisconnectReply {} => {
-                    end = true;
-                    Ok(())
-                }
-                Request::Error { code, detail, .. } => Err(ClientError::XimError(code, detail)),
-                Request::ForwardEvent { xev, .. } => {
-                    let xev = &xev[..];
-                    if let Ok(press) = KeyPressEvent::try_from(xev) {
-                        log::info!("forward press: 0x{:X}", press.detail);
-                        Ok(())
-                    } else {
-                        Err(ClientError::InvalidReply)
-                    }
-                }
-                Request::Commit {
-                    input_method_id,
-                    input_context_id,
-                    data,
-                } => match data {
-                    CommitData::Keysym { keysym, .. } => {
-                        log::info!("Commited keysym: {}", keysym);
-
-                        Ok(())
-                    }
-                    CommitData::Chars { commited, .. } => {
-                        log::info!(
-                            "Commited {}",
-                            ctext::compound_text_to_utf8(&commited).unwrap()
-                        );
-
-                        Ok(())
-                    }
-                    _ => todo!(),
-                },
-                _ => Err(ClientError::InvalidReply),
-            }
-        })? {
+        if client.filter_event(&e, &mut handler)? {
             log::trace!("event consumed");
         } else if let Event::Error(err) = e {
             return Err(ClientError::X11Error(err).into());
         } else {
             match e {
-                Event::KeyPress(e) | Event::KeyRelease(e) => match (im_id, ic_id) {
-                    (Some(im), Some(ic)) => {
-                        client.forward_key_press(im, ic, &e)?;
+                Event::KeyPress(e) | Event::KeyRelease(e) => {
+                    if handler.connected {
+                        client.forward_event(
+                            handler.im_id,
+                            handler.ic_id,
+                            ForwardEventFlag::empty(),
+                            e.sequence,
+                            e.into(),
+                        )?;
                     }
-                    _ => {}
-                },
+                }
                 _ => {}
             }
         }
     }
-
-    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,82 @@ pub mod x11rb;
 use std::collections::HashMap;
 use std::iter;
 
-use xim_parser::{Attribute, AttributeName, Writer, XimWrite};
+use xim_parser::{
+    Attribute, AttributeName, Extension, ForwardEventFlag, RawXEvent, Writer, XimWrite,
+};
+
+pub trait Client {
+    type Error: std::fmt::Debug + std::fmt::Display + std::error::Error;
+
+    fn build_ic_attributes(&self) -> AttributeBuilder;
+    fn build_im_attributes(&self) -> AttributeBuilder;
+
+    fn disconnect(&mut self) -> Result<(), Self::Error>;
+    fn open(&mut self, locale: &[u8]) -> Result<(), Self::Error>;
+    fn close(&mut self, input_method_id: u16) -> Result<(), Self::Error>;
+    fn quert_extension(
+        &mut self,
+        input_method_id: u16,
+        extensions: &[&str],
+    ) -> Result<(), Self::Error>;
+    fn create_ic(
+        &mut self,
+        input_method_id: u16,
+        ic_attributes: Vec<Attribute>,
+    ) -> Result<(), Self::Error>;
+    fn destory_ic(
+        &mut self,
+        input_method_id: u16,
+        input_context_id: u16,
+    ) -> Result<(), Self::Error>;
+    fn forward_event(
+        &mut self,
+        input_method_id: u16,
+        input_context_id: u16,
+        flag: ForwardEventFlag,
+        sequence: u16,
+        xev: RawXEvent,
+    ) -> Result<(), Self::Error>;
+}
+
+pub trait ClientHandler<C: Client> {
+    fn handle_connect(&mut self, client: &mut C) -> Result<(), C::Error>;
+    fn handle_disconnect(&mut self);
+    fn handle_open(&mut self, client: &mut C, input_method_id: u16) -> Result<(), C::Error>;
+    fn handle_close(&mut self, client: &mut C, input_method_id: u16) -> Result<(), C::Error>;
+    fn handle_query_extension(
+        &mut self,
+        client: &mut C,
+        extensions: &[Extension],
+    ) -> Result<(), C::Error>;
+    fn handle_create_ic(
+        &mut self,
+        client: &mut C,
+        input_method_id: u16,
+        input_context_id: u16,
+    ) -> Result<(), C::Error>;
+    fn handle_destory_ic(
+        &mut self,
+        client: &mut C,
+        input_method_id: u16,
+        input_context_id: u16,
+    ) -> Result<(), C::Error>;
+    fn handle_commit(
+        &mut self,
+        client: &mut C,
+        input_method_id: u16,
+        input_context_id: u16,
+        text: &str,
+    ) -> Result<(), C::Error>;
+    fn handle_forward_event(
+        &mut self,
+        client: &mut C,
+        input_method_id: u16,
+        input_context_id: u16,
+        flag: ForwardEventFlag,
+        xev: RawXEvent,
+    ) -> Result<(), C::Error>;
+}
 
 pub use ctext::{compound_text_to_utf8, utf8_to_compound_text};
 
